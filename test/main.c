9bfb43d80e953bf717bdaa27198490e8928d7986
Ignatius <ice.emissary@gmail.com>
2016-11-23 18:04:47 +0800

    [add] 加入開發測試程式。

---
+++ test/main.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include <stdio.h>
+#include "tlog.h"
+
+int main(void)
+{
+    static const char modname[] = "tinylog_test";
+
+    tlog_set_loglevel(TLOG_LEV_WARN);
+    tlog_set_logfile("./testlog.log");
+
+    assert( 0 == tlog_print_fail (modname, "Sample int=%d, str=%s.", 3, "text") );
+    assert( 0 == tlog_print_error(modname, "Sample int=%d, str=%s.", 3, "text") );
+    assert( 0 == tlog_print_warn (modname, "Sample int=%d, str=%s.", 3, "text") );
+    assert( 0 == tlog_print_note (modname, "Sample int=%d, str=%s.", 3, "text") );
+    assert( 0 == tlog_print_info (modname, "Sample int=%d, str=%s.", 3, "text") );
+    assert( 0 == tlog_print_debug(modname, "Sample int=%d, str=%s.", 3, "text") );
+
+    printf("Log file generated. Now please check the file manually.\n");
+
+    return 0;
+}
