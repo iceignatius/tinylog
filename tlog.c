298da0d51e50eb78ea4d2cd1e487a0f5b802803b
Ignatius <ice.emissary@gmail.com>
2016-11-23 18:05:07 +0800

    [add] 完成 tlog 模組除了輸出記錄訊息以外的大部份功能。

--- tlog.c
+++ tlog.c
@@ -1,4 +1,36 @@
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <gen/ascii.h>
+#include <gen/jmpbk.h>
+#include <gen/bufstm.h>
+#include <gen/systime.h>
+#include <gen/timeinf.h>
 #include "tlog.h"
+
+typedef struct record_t
+{
+    long long   uxtime;
+    const char *modname;
+    const char *funcname;
+    unsigned    level;
+    char        msg[1024];
+} record_t;
+
+typedef struct log_param_t
+{
+    unsigned level;
+    int      targets;
+    char     logfile[1024];
+} log_param_t;
+
+static log_param_t logparam =
+{
+    .level   = TLOG_LEV_INFO,
+    .targets = TLOG_OUTPUT_STDOUT | TLOG_OUTPUT_LOGFILE,
+    .logfile = "/var/log/tlog.log",
+};
 
 //------------------------------------------------------------------------------
 void tlog_set_loglevel(unsigned level)
@@ -7,9 +39,10 @@ void tlog_set_loglevel(unsigned level)
      * Change the record filter level.
      *
      * @param level It can be any level that defined in ::tlog_lev_t,
-     *              and any records which level are great then the value will be discared.
+     *              and any records which level are great then the value will be discard.
      *              The default value is ::TLOG_LEV_INFO.
      */
+    logparam.level = level;
 }
 //------------------------------------------------------------------------------
 void tlog_set_output(int targets)
@@ -19,8 +52,9 @@ void tlog_set_output(int targets)
      *
      * @param targets The combination of ::tlog_output_t,
      *                and that indicated where the log message should be output to.
-     *                The default compination is TLOG_OUTPUT_STDOUT | TLOG_OUTPUT_LOGFILE.
+     *                The default combination is TLOG_OUTPUT_STDOUT | TLOG_OUTPUT_LOGFILE.
      */
+    logparam.targets = targets;
 }
 //------------------------------------------------------------------------------
 void tlog_set_logfile(const char *filename)
@@ -31,6 +65,86 @@ void tlog_set_logfile(const char *filename)
      * @param filename The path and name of the log file.
      *                 The default is "/var/log/tlog.log".
      */
+    strncpy(logparam.logfile, filename, sizeof(logparam.logfile)-1);
+}
+//------------------------------------------------------------------------------
+static
+void record_collect_info(record_t   *record,
+                         const char *module,
+                         const char *funcname,
+                         unsigned    level,
+                         const char *msg_format,
+                         va_list     msg_args)
+{
+    record->uxtime   = systime_get_local();
+    record->modname  = module;
+    record->funcname = funcname;
+    record->level    = level;
+    vsnprintf(record->msg, sizeof(record->msg), msg_format, msg_args);
+}
+//------------------------------------------------------------------------------
+static
+const char* get_level_str(unsigned level)
+{
+    switch( level )
+    {
+    case TLOG_LEV_FAIL  :   return "FAIL";
+    case TLOG_LEV_ERROR :   return "ERROR";
+    case TLOG_LEV_WARN  :   return "WARN";
+    case TLOG_LEV_NOTE  :   return "NOTE";
+    case TLOG_LEV_INFO  :   return "INFO";
+    case TLOG_LEV_DEBUG :   return "DEBUG";
+    default             :   return "";
+    }
+}
+//------------------------------------------------------------------------------
+static
+void replace_ascii_ctrl_to_star(char *str)
+{
+    for(; *str; ++str)
+    {
+        if( iscntrl(*str) )
+            *str = '*';
+    }
+}
+//------------------------------------------------------------------------------
+static
+bool push_record_item(bufostm_t *stream, const char *tag, const char *content, bool is_last)
+{
+    if( !bufostm_write(stream, tag, strlen(tag)) ) return false;
+    if( !bufostm_write(stream, ": ", 2) ) return false;
+    if( !bufostm_write(stream, content, strlen(content)) ) return false;
+    if( !bufostm_putbyte(stream, ( is_last ? ASCII_LF : ASCII_HT )) ) return false;
+
+    return true;
+}
+//------------------------------------------------------------------------------
+static
+bool record_encode_str(record_t *record, char *buf, size_t bufsize)
+{
+    bufostm_t stream;
+    bufostm_init(&stream, buf, bufsize);
+
+    bool res = false;
+    do
+    {
+        char content[sizeof(record->msg)] = {0};
+
+        timeinf_t timeinf = timeinf_from_uxtime(record->uxtime);
+        timeinf_to_datetimestr(&timeinf, content, sizeof(content)-1, "/", ":", " ");
+        if( !push_record_item(&stream, "time", content, false) ) break;
+
+        if( !push_record_item(&stream, "mod", record->modname, false) ) break;
+        if( !push_record_item(&stream, "func", record->funcname, false) ) break;
+        if( !push_record_item(&stream, "lev", get_level_str(record->level), false) ) break;
+
+        replace_ascii_ctrl_to_star(record->msg);
+        if( !push_record_item(&stream, "msg", record->msg, true) ) break;
+
+        res = true;
+    } while(false);
+
+    return res;
 }
 //------------------------------------------------------------------------------
 int tlog_print_detail(const char *module, const char *func, unsigned level, const char *format, ...)
@@ -45,5 +159,28 @@ int tlog_print_detail(const char *module, const char *func, unsigned level, cons
      * @param ...    Arguments that just like in "printf".
      * @return ZERO if success; and others if failed.
      */
+    if( level > logparam.level ) return 0;
+
+    va_list msg_args;
+    va_start(msg_args, format);
+
+    int res;
+    JMPBK_BEGIN
+    {
+        record_t record;
+        record_collect_info(&record, module, func, level, format, msg_args);
+
+        char recstr[2*sizeof(record.msg)] = {0};
+        if( !record_encode_str(&record, recstr, sizeof(recstr)-1) ) JMPBK_THROW(0);
+    }
+    JMPBK_FINAL
+    {
+        res = JMPBK_ERRCODE;
+    }
+    JMPBK_END
+
+    va_end(msg_args);
+
+    return res;
 }
 //------------------------------------------------------------------------------
